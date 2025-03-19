07353794cf91c338bd7210ee44e27a126c5b338f
SBurlikJr <101330781+SBurlikJr@users.noreply.github.com>
2025-03-19 23:11:03 +0200

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,75 +1,51 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=postgres -t -c"
 
-PSQL="psql --username=freecodecamp --dbname=postgres -t --no-align -c"
+echo -e "\nEnter your username:"
+read USERNAME
 
-DISPLAY() {
-  echo -e "\n~~~~~ Number Guessing Game ~~~~~\n" 
-
-  #get username
-  echo "Enter your username:"
-  read USERNAME
-
-  #get username from db
-  USER_ID=$($PSQL "select user_id from users where username = '$USERNAME'")
-
-  #if user present
-  if [[ $USER_ID ]]; then
-    #get games played
-    GAMES_PLAYED=$($PSQL "select count(user_id) from games where user_id = '$USER_ID'")
-
-    #get best game (guess)
-    BEST_GUESS=$($PSQL "select min(number_of_guesses) from games where user_id = '$USER_ID'")
-
-    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GUESS guesses."
+if [[ ${#USERNAME} -gt 22 ]]
+then
+  echo "This username is too long (>22 characters)."
+else
+  USERNAME_RESULT=$($PSQL "select * from users where username='$USERNAME'")
+  # no user; create
+  if [[ -z $USERNAME_RESULT ]]
+  then
+    NEW_USER=$($PSQL "insert into users(username) values('$USERNAME')")
+    echo -e "\nWelcome, $(echo $USERNAME | sed -E 's/^ *| *$//g')! It looks like this is your first time here."
   else
-    #if u_name not present in db
-    echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
-
-    #insert to users table
-    INSERTED_TO_USERS=$($PSQL "insert into users(username) values('$USERNAME')")
-    #get user_id
-    USER_ID=$($PSQL "select user_id from users where username = '$USERNAME'")
-    # echo $USER_ID
+    # pull user's game data
+    USER_GAME_DATA=$($PSQL "select user_id, username, count(*) as number_of_games, (select min(number_of_guesses) from games) as minimum_num_guess from games inner join users using(user_id) where username='$USERNAME' group by user_id, username")
+    echo "$USER_GAME_DATA" | while read USER_ID BAR USERNAME BAR NUM_OF_GAMES BAR BEST_GUESS_NUM
+    do
+      echo -e "\nWelcome, $(echo $USERNAME | sed -E 's/^ *| *$//g')! You have played $(echo $NUM_OF_GAMES | sed -E 's/^ *| *$//g') games, and your best game took $(echo $BEST_GUESS_NUM | sed -E 's/^ *| *$//g') guesses."
+    done
   fi
 
-  GAME
-}
+  RANDOM_GENERATED_NUMBER=$(($RANDOM%1000 + 1))
+  NUMBER_OF_GUESSES=0
 
-GAME() {
-  #secret number
-  SECRET=$((1 + $RANDOM % 1000))
-
-  #count number_of_guesses
-  TRIES=0
-
-  #guess number
-  # echo $SECRET
-  GUESSED=0
   echo -e "\nGuess the secret number between 1 and 1000:"
 
-  while [[ $GUESSED = 0 ]]; do
-    read GUESS
-
-    #if not a number
-    if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
-      echo -e "\nThat is not an integer, guess again:"
-    #if correct guess
-    elif [[ $SECRET = $GUESS ]]; then
-      TRIES=$(($TRIES + 1))
-      echo -e "\nYou guessed it in $TRIES tries. The secret number was $SECRET. Nice job!"
-      #insert into db
-      INSERTED_TO_GAMES=$($PSQL "insert into games(user_id, number_of_guesses, rand_generated_num) values($USER_ID, $TRIES, $SECRET)")
-      GUESSED=1
-    #if greater
-    elif [[ $SECRET -gt $GUESS ]]; then
-      TRIES=$(($TRIES + 1))
-      echo -e "\nIt's higher than that, guess again:"
-    #if smaller
-    else
-      TRIES=$(($TRIES + 1))
-      echo -e "\nIt's lower than that, guess again:"
+  while true
+  do
+    ((NUMBER_OF_GUESSES+=1))
+    read USER_INPUT
+    if [[ ! $USER_INPUT =~ ^[0-9]+$ ]]
+    then
+      echo -e "That is not an integer, guess again:\n"
+    elif [[ $USER_INPUT > $RANDOM_GENERATED_NUMBER ]]
+    then
+      echo -e "It's lower than that, guess again:\n"
+    elif [[ $USER_INPUT < $RANDOM_GENERATED_NUMBER ]]
+    then
+      echo -e "It's higher than that, guess again:\n"
+    else 
+      echo -e "\nYou guessed it in $(echo $NUMBER_OF_GUESSES | sed -E 's/^ *| *$//g') tries. The secret number was $(echo $RANDOM_GENERATED_NUMBER | sed -E 's/^ *| *$//g'). Nice job!"
+      break
     fi
   done
-}
-
-DISPLAY
+  USER_ID=$($PSQL "select user_id from users where username='$USERNAME'")
+  GAME_RECORDS=$($PSQL "insert into games(user_id, rand_generated_num, number_of_guesses) values($USER_ID,$RANDOM_GENERATED_NUMBER,$NUMBER_OF_GUESSES)")
+fi
